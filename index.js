552f37fc889cb883c7afe37408d8f8d01ccffde4
liz <rhodes.liz@gmail.com>
2022-07-19 17:54:01 -0400

    commit tree 340676a4125253a6cf3f82af46979f09c063705b

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+return drivers.slice(0,2)
+}
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(2)
+    }
+
+    const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+    function createFareMultiplier(fare) {
+        return function(multiplyer) {
+           return fare * multiplyer
+            }
+    }
+    // const fareDoubler = (fare) => {
+    //     return function(multiplier) {
+    //         return fare * multiplier
+
+    const fareDoubler = createFareMultiplier(2)
+    const fareTripler = createFareMultiplier(3)
+
+    // function selectDifferentDrivers(Drivers, returnFirstTwoDrivers()) {
+    //     return selectDifferentDrivers
+    
+    function selectDifferentDrivers(arrayOfDrivers, driversFunction) {
+       return driversFunction(arrayOfDrivers)
+    }
